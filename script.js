192adb3079d3454596ac4d1cc7570ce3c1561349
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-04 13:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+const symbol = [
+    ['M',1000],
+    ['CM',900],
+    ['D', 500], 
+    ['CD', 400], 
+    ['C', 100], 
+    ['XC', 90], 
+    ['L', 50], 
+    ['XL', 40], 
+    ['X', 10], 
+    ['IX', 9], 
+    ['V', 5], 
+    ['IV', 4], 
+    ['I', 1]
+]
 
-  //your code here
+let num = 18; 
+// 700 + 90 + 8
+// 500 + 100 + 100 + 90 + 5 + 1 + 1 + 1
+//DCCXCVIII
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+let result = "";
 
-// console.log(convertToRoman(36));
+symbol.forEach((item) => {
+    // console.log(item);
+    // let val1 = item[0];
+    // let val2 = item[1];
+    // console.log(val1, val2);
+    const [key, val] = item; //destructuring of array
+    // console.log(key, val);
 
+    while(num >= val) {
+        num -= val;
+        result += key;
+    }
+})
 
-
-
-// do not edit below this line
-module.exports = convertToRoman
+console.log(result);
